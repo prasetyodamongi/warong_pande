bcac0a7302d053c40e5ee9fa4b045036543d34b2
unknown <damongi.yermia@gmail.com>
2022-07-02 18:51:03 +0700

    commit pertama

---
+++ app/src/main/java/com/pcoding/warongpande/BluetoothprintClass.java
@@ -0,0 +1,12 @@
+package com.pcoding.warongpande;
+
+import android.app.Application;
+
+public class BluetoothprintClass extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
