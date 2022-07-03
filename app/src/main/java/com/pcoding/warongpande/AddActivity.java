36d7e017271798dcf89f05280381437929c4df5f
pdamongi <damongi.yermia@gmail.com>
2022-07-03 19:33:47 +0700

    commit kelima @prasetyodamongi

---
+++ app/src/main/java/com/pcoding/warongpande/AddActivity.java
@@ -0,0 +1,27 @@
+package com.pcoding.warongpande;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/pcoding/warongpande/MainActivity.java
+++ app/src/main/java/com/pcoding/warongpande/MainActivity.java
@@ -1,8 +1,11 @@
 package com.pcoding.warongpande;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CardView add_pesan = (CardView) findViewById(R.id.add_menu);
+        add_pesan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
