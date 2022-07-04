18706d415df59845e72a7476fb0b8b75e6c24660
pdamongi <damongi.yermia@gmail.com>
2022-07-04 19:55:26 +0700

    commit keenam @prasetyodamongi

--- app/src/main/java/com/pcoding/warongpande/MainActivity.java
+++ app/src/main/java/com/pcoding/warongpande/MainActivity.java
@@ -1,25 +1,71 @@
 package com.pcoding.warongpande;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CardView add_pesan = (CardView) findViewById(R.id.add_menu);
-        add_pesan.setOnClickListener(new View.OnClickListener() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView =findViewById(R.id.navigation);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        bottomNavigationView = findViewById(R.id.bar_bottom_menu);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentBeranda()).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener(){
+            @SuppressLint("NonConstantResourceId")
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, AddActivity.class);
-                startActivity(intent);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.beranda:
+                        selectedFragment = new FragmentBeranda();
+                        break;
+                    case R.id.list:
+                        selectedFragment = new FragmentListPesanan();
+                        break;
+                    case R.id.history:
+                        selectedFragment = new FragmentHistory();
+                        break;
+                    case R.id.akun:
+                        selectedFragment = new FragmentAkun();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                return true;
             }
         });
 
